2e2639fc879c9f9cfef9f7defca1de3da6bc0193
Ab Akam <abrahamakam@gmail.com>
2018-01-27 19:38:10 +0100

    remove server.log

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const hbs = require('hbs');
 const helpers = require('./utils/helpers');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -17,9 +19,9 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    res.render('maintenance.hbs');
-})
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// })
 
 app.use(express.static(__dirname + '/public'));
 
@@ -42,6 +44,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server up on port: 3000');
+app.listen(port, () => {
+    console.log(`Server up on port: ${port}`);
 });
